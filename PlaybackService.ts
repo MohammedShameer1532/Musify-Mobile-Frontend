c888e5f03def0a9eb9efa5a7163d6043f958a2ed
Mohammed Shameer <mshameer260@gmail,com>
2026-04-17 15:17:28 +0530

    remove unwanted code

--- PlaybackService.ts
+++ PlaybackService.ts
@@ -5,7 +5,6 @@ export const PlaybackService = async () => {
   const subscriptions = [
     TrackPlayer.addEventListener(Event.RemotePlay, TrackPlayer.play),
     TrackPlayer.addEventListener(Event.RemotePause, TrackPlayer.pause),
-    TrackPlayer.addEventListener(Event.RemoteStop, TrackPlayer.stop),
 
     TrackPlayer.addEventListener(Event.RemoteNext, async () => {
       try {
